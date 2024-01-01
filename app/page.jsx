434a6005e4a97c1b483e58d665dcf02cafb41c5f
emredkyc <emredkyc@hotmail.com>
2024-01-01 18:22:23 +0100

    create home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import Feed from "@components/Feed";
 
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+const Home = () => (
+  <section className='w-full flex-center flex-col'>
+    <h1 className='head_text text-center'>
+      Discover & Share
+      <br className='max-md:hidden' />
+      <span className='orange_gradient text-center'> AI-Powered Prompts</span>
+    </h1>
+    <p className='desc text-center'>
+      Promptify is an open-source AI prompting tool for modern world to
+      discover, create and share creative prompts
+    </p>
 
-export default Home
+    <Feed />
+  </section>
+);
+
+export default Home;
