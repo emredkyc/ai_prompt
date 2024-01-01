dee1514e22d2fdcd29ec6739d5e8ae71e68bbb35
emredkyc <emredkyc@hotmail.com>
2024-01-01 20:43:11 +0100

    update provider component

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+"use client";
 
-const Provider = () => {
-  return (
-    <div>Provider</div>
-  )
-}
+import { SessionProvider } from "next-auth/react";
 
-export default Provider
+const Provider = ({ children, session }) => (
+  <SessionProvider session={session}>
+    {children}
+  </SessionProvider>
+);
+
+export default Provider;
